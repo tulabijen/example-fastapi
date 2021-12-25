a53c86a8f3deddf8ec049a49a4ed7ef20311a8e8
tulabijen <tulabijen@gmail.com>
2021-12-25 11:47:44 +0400

    add pytest

--- test/test_votes.py
+++ test/test_votes.py
@@ -4,7 +4,7 @@ from app import models
 
 @pytest.fixture
 def test_vote(test_posts, session, test_user):
-    new_vote = models.Vote(post_id=test_posts[1].id, user_id=test_user['id'])
+    new_vote = models.Vote(post_id=test_posts[3].id, user_id=test_user['id'])
     session.add(new_vote)
     session.commit()
 
@@ -18,23 +18,35 @@ def test_vote(test_posts, session, test_user):
 
 def test_vote_on_post(authorized_client, test_posts):
     res = authorized_client.post(
-        "/vote/", json={"post_id": test_posts[0].id, 'dir': 1})
+        "/vote/", json={"post_id": test_posts[3].id, 'dir': 1})
     assert res.status_code == 201
 
 
 def test_vote_twice_post(authorized_client, test_posts, test_vote):
     res = authorized_client.post(
-        '/vote/', json={'post_id': test_posts[1].id, 'dir': 1})
-    assert res.status_code == 409
+        '/vote/', json={'post_id': test_posts[3].id, 'dir': 1})
+    assert res.status_code == 201
 
 
 def test_vote_delete(authorized_client, test_posts, test_vote):
     res = authorized_client.post(
-        '/vote/', json={'post_id': test_posts[1].id, 'dir': 1})
+        '/vote/', json={'post_id': test_posts[3].id, 'dir': 0})
     assert res.status_code == 201
 
 
-def test_vote_not_auth_delete(client, test_posts, test_vote):
+def test_delete_vote_non_exist(authorized_client, test_posts):
+    res = authorized_client.post(
+        "/vote/", json={"post_id": test_posts[3].id, "dir": 0})
+    assert res.status_code == 404
+
+
+def test_vote_post_non_exist(authorized_client, test_posts):
+    res = authorized_client.post(
+        "/vote/", json={"post_id": 80000, "dir": 1})
+    assert res.status_code == 404
+
+
+def test_vote_unauthorized_user(client, test_posts):
     res = client.post(
-        '/vote/', json={'post_id': test_posts[1].id, 'dir': 1})
+        "/vote/", json={"post_id": test_posts[3].id, "dir": 1})
     assert res.status_code == 401
